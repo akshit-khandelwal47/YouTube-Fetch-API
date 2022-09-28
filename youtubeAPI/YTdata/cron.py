a8a89f3a9aa1f27b320b9963208c69fa012ff5d7
akshit-khandelwal47 <akshitkhandelwal20@gmail.com>
2022-09-28 18:39:55 +0530

    API Code

--- youtubeAPI/YTdata/cron.py
+++ youtubeAPI/YTdata/cron.py
@@ -3,65 +3,84 @@ import os
 from django_cron import CronJobBase, Schedule
 
 # Google API
-from apiclient import build
+from googleapiclient.discovery import build
 import apiclient
 
 from .models import *
 from youtubeAPI import settings
 from datetime import datetime, timedelta
 
-
+#CronJobBase is used for recusively calling the api.
 class CallYoutubeApi(CronJobBase):
-    RUN_EVERY_MINS = 5  # runs after every 5 minutes
+    RUN_EVERY_SECS = 10  
 
-    schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
-    code = 'YTdata.call_youtube_api'    # a unique code
+    schedule = Schedule(run_every_mins=RUN_EVERY_SECS)
+    code = 'YTdata.call_youtube_api'    
 
-    def do(self):
-        apiKeys = settings.GOOGLE_API_KEYS
-        time_now = datetime.now()
-        last_request_time = time_now - timedelta(minutes=5)
+    try:
+        def do(self):
+            apiKeys = settings.GOOGLE_API_KEYS
+            time_now = datetime.now()
+            last_request_time = time_now - timedelta(seconds=10)  #for calling the api in every 10 sec
 
-        #####################################################
+            # A variable to check if a valid api key exists or not
+            valid = False
 
-        # A variable to check if a valid api key exists or not
-        valid = False
+            for apiKey in apiKeys:
+                try:
+                    youtube = build("youtube", "v3", developerKey=apiKey)
+                    req = youtube.search().list(q="cricket", part="snippet", order="date", maxResults=50,
+                                                publishedAfter=(last_request_time.replace(microsecond=0).isoformat()+'Z'))
+                    res = req.execute()
+                    print(res)
+                    valid = True
+                except apiclient.errors.HttpError as err:
+                    code = err.resp.status
+                    if not(code == 400 or code == 403):
+                        break
 
-        for apiKey in apiKeys:
-            try:
-                youtube = build("youtube", "v3", developerKey=apiKey)
-                req = youtube.search().list(q="cricket", part="snippet", order="date", maxResults=50,
-                                            publishedAfter=(last_request_time.replace(microsecond=0).isoformat()+'Z'))
-                res = req.execute()
-                print(res)
-                valid = True
-            except apiclient.errors.HttpError as err:
-                code = err.resp.status
-                if not(code == 400 or code == 403):
+                if valid:
                     break
 
+
             if valid:
-                break
 
 
-        if valid:
-
-
-            for item in res['items']:
-                video_id = item['id']['videoId']
-                published_datetime = item['snippet']['publishedAt']
-                video_title = item['snippet']['videoTitle']
-                description = item['snippet']['description']
-                thumbnail_urls = item['snippet']['thumbnail']['default']['url']
-                channel_id = item['snippet']['channelId']
-                channel_title = item['snippet']['channelTitle']
-                print(video_title)
-                VideoData.objects.create(
-                    video_id=video_id,
-                    video_title=video_title,
-                    description=description,
-                    channel_id=channel_id,
-                    channel_title=channel_title,
-                    published_date_time=published_datetime,
-                    thumbnail_urls=thumbnail_urls,
+                # for item in res['items']:
+                #     video_id = item['id']['videoId']
+                #     published_datetime = item['snippet']['publishedAt']
+                #     video_title = item['snippet']['videoTitle']
+                #     description = item['snippet']['description']
+                #     thumbnail_urls = item['snippet']['thumbnail']['default']['url']
+                #     channel_id = item['snippet']['channelId']
+                #     channel_title = item['snippet']['channelTitle']
+                #     print(video_title)
+                #     VideoData.objects.create(
+                #         video_id=video_id,
+                #         video_title=video_title,
+                #         description=description,
+                #         channel_id=channel_id,
+                #         channel_title=channel_title,
+                #         published_date_time=published_datetime,
+                #         thumbnail_urls=thumbnail_urls,
+                #     )
+                for item in res['items']:
+                    video_id = item['id']['videoId']
+                    publishedDateTime = item['snippet']['publishedAt']
+                    title = item['snippet']['title']
+                    description = item['snippet']['description']
+                    thumbnailsUrls = item['snippet']['thumbnails']['default']['url']
+                    channel_id = item['snippet']['channelId']
+                    channel_title = item['snippet']['channelTitle']
+                    print(title)
+                    VideoData.objects.create(
+                        video_id=video_id,
+                        title=title,
+                        description=description,
+                        channel_id=channel_id,
+                        channel_title=channel_title,
+                        publishedDateTime=publishedDateTime,
+                        thumbnailsUrls=thumbnailsUrls,
                 )
+    except:
+        print("Error found")
--- youtubeAPI/YTdata/views.py
+++ youtubeAPI/YTdata/views.py
@@ -5,19 +5,25 @@ from YTdata.models import *
 from YTdata.serializers import *
 from rest_framework import generics
 from rest_framework.pagination import CursorPagination
+from django.http import HttpResponse
 
 
 class PaginationRes(CursorPagination):
-    page_size = 25
+    page_size = 25 #fetch results upto 25 pages
     page_size_query_param = page_size
     max_page_size = 120
 
 class YouTubeVid(generics.ListAPIView):
     
-    search_field = ['video_title','description']
+    search_field = ['title','description']
     filter_backends = (filters.SearchFilter,DjangoFilterBackend,filters.OrderingFilter)
     filterset_field = ['channel_id','channel_title']
-    ordering = ('-published_datetime')
+    ordering = ('-publishedDateTime')
     queryset = VideoData.objects.all()
     serializer_class = VideoDataSerializers
     pagination_class = PaginationRes
+
+
+
+    
+
